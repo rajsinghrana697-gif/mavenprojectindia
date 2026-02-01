0ff4db73c8573bc38d1666e00236a142b461feed
RAJ SINGH RANA <rajsinghrana697@gmail.com>
2026-02-01 20:35:39 +0530

    Student

---
+++ src/main/java/com/rana/Hello.java
@@ -0,0 +1,14 @@
+package com.rana;
+
+public class Hello {
+    public static void main(String [] args){
+        Student student = new Student();
+                student.setName("Raj singh rana");
+                        student.setAge(26);
+        System.out.println(student.getName());
+        System.out.println(student.getAge()  );
+
+
+    }
+
+}
