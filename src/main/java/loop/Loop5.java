e1db3060bc1f06d32ee272cab1122bb2bea6a8da
RAJ SINGH RANA <rajsinghrana697@gmail.com>
2026-02-10 15:07:56 +0530

    Loop

---
+++ src/main/java/loop/Loop5.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class Loop5 {
+    public static void main(String[] args) {
+        for (int i=0; i<100;i++){
+            if (i==70) {
+                break;
+            }
+                System.out.println(i);
+            }
+        }
+    }
